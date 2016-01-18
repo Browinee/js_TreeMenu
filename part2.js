b9d0183819c6340033d1a41d6ea3f2915281c4ac
Daniel <daniel.chiang@isapsolution.com>
2016-01-18 17:59:27 +0800

    long~

---
+++ part2.js
@@ -0,0 +1,115 @@
+"use strict";
+
+var data = [
+  { name: "Animal", data: [
+                  { name: "Mammal", data: [
+                          { name: "Equine", data: [
+                                  { name: "Horse", data: null },
+                                  { name: "Zebra", data: null }
+                                  ] },
+                          { name: "Bovine", data: [
+                                  { name: "Cow" }
+                                  ] },
+                          { name: "Canine", data: [
+                                  { name: "Lassie" },
+                                  { name: "Rintlntin" }
+                                  ] }
+                          ] },
+                  { name: "Reptlle", data: [
+                          { name: "Lizard", data: [
+                                  { name: "Salamander" }
+                                  ] },
+                          { name: "Snake", data: null },
+                          { name: "Bird", data: [
+                                  { name: "Canary" }
+                                  ] }
+                          ] },
+          ] },
+  { name: "Human", data: [] }
+];
+
+
+
+//---------------------------------------------------------------------------------------
+
+
+
+function printAndRetrieve(obj, position) {
+    //var theBody = document.getElementsByTagName("body")[0];
+  /*
+    if (obj[0].data) {
+        theBody.appendChild(ul);
+        ul.appendChild(li);
+    } else {
+        obj.parentNode.appendChild(ul);
+        ul.appendChild(li);
+    }
+
+    if (obj[i].data[i] != null) printAndRetrieve(obj[i].data, i+1);
+    */
+    for (var i = 0; i < obj.length; i++) {
+console.log(obj[i].name);
+        var ul = document.createElement("ul");
+        var li = document.createElement("li");
+        if ( position.getElementsByTagName('ul').length == 0 ) {
+            position.appendChild(ul);
+        }
+        li.innerHTML = obj[i].name;
+        ul.appendChild(li);
+console.log("----------------------------");
+        if (obj[i].data != null) {
+            printAndRetrieve(obj[i].data, li);
+        }
+        else {
+            if (obj[i+1].data != null)
+                printAndRetrieve(obj[i+1].data, li);
+        }
+    }
+
+}
+
+function printList(key, value) {
+    var myBody = document.getElementsByTagName("body")[0];
+    var ul = document.createElement("ul");
+    var li = document.createElement("li");
+
+    //if ( "name" == key ) {
+
+
+    if ( "name" == key && myBody.getElementsByTagName('ul').length == 0) {
+        myBody.appendChild(ul);
+    } else {
+        if ( "name" == key ) {
+            var ulAll = document.getElementsByTagName("ul");
+            ulAll[ulAll.length - 1].appendChild(ul);
+        // u = document.getElementsByTagName("ul")[0];
+        // u.appendChild(ul);
+        } else {
+
+        }
+    }
+    li.innerHTML = value;
+    ul.appendChild(li);
+    //}
+}
+
+//printAndRetrieve(data, document.getElementsByTagName("body")[0]);
+
+
+function process(key,value) {
+    console.log(key + " : "+value);
+}
+var q=0;
+function traverse(obj,func) {
+    for (var i in obj) {
+        func.apply( this, [i, obj[i] ] );
+        if (obj[i] !== null && typeof(obj[i])=="object") {
+            console.log("========================Round: "+q+" | i= "+i+" | val="+obj[i]+"========================"); q++;
+            //going on step down in the object tree!!
+            traverse(obj[i],func);
+        }
+    }
+}
+
+traverse(data,printList);
+// traverse(data,process);
