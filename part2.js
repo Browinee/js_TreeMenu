2845cd50a9335b655b70999dbf7298b5c5e234be
Daniel <daniel.chiang@isapsolution.com>
2016-01-19 10:12:36 +0800

    change part2.js(switch)

--- part2.js
+++ part2.js
@@ -68,37 +68,143 @@ console.log("----------------------------");
 
 }
 
+//------------------------------
+var lengthSub = 1;
+var layer = 0;
+var nullData = false;
+var passNumbers = false;  // pass this round if key == 1 or 2
+var tmpName, rollback;
+//------------------------------
+
 function printList(key, value) {
     var myBody = document.getElementsByTagName("body")[0];
     var ul = document.createElement("ul");
     var li = document.createElement("li");
     var ulAll = document.getElementsByTagName("ul");
-    var lengthSub = 1;
+    var liAll = document.getElementsByTagName("li");
+
 
     if ( myBody.getElementsByTagName('ul').length == 0 ) {
+    //set root ul
         myBody.appendChild(ul);
         ul.appendChild(li);
         console.log("Add body");
         return;
     } else {
+        switch (key) {
+            case "0":
+                console.log("0 == key, add ul, the val is: "+value);
+                //---------------------------
+                //ulAll[ulAll.length - 1].appendChild(ul);
+                liAll[liAll.length - 1].appendChild(ul);
+                console.log("Now we got "+ulAll.length+" ul");
+                //---------------------------
+                ul.appendChild(li);
+                layer += 1;
+                break;
+            case "name":
+                tmpName = value;
+                console.log("set tmpName ="+tmpName);
+                //---------------------------
+                break;
+            case "data":
+                if ( value == null ) {  //at end-point
+                    if (nullData) // Last one is a sibling, add li
+                        liAll[liAll.length - 1].appendChild(li);
+                    console.log("add child for null");
+                    //---------------------------
+                    addNameIntoLi( liAll[liAll.length - 1] );
+                    rollback = true;
+                    passNumbers = true;
+                    nullData = true;
+                } else {
+                    nullData = false;
+                     if (rollback) {
+                     //it's parents node
+                          console.log("let's rise!");
+                          //---------------------------
+                          var LiTarget = document.getElementsByTagName("li")[0];
+                          for ( var i = 0; i < layer - 1; i++ ) {
+                              console.log("Now, i="+i+", layer = "+layer);
+                              LiTarget = LiTarget.getElementsByTagName("li")[0];
+                              console.log(LiTarget);
+                            }
+                          //addNameIntoLi(LiTarget);
+                          var nameNode = document.createTextNode(tmpName);
+                          li.appendChild(nameNode);
+                          insertAfter(li, LiTarget);
+                          layer -= 1;
+                     } else {
+                        addNameIntoLi( liAll[liAll.length - 1] );
+                        console.log("add name!");
+                          //---------------------------
+                     }
+                }
+                break;
+            case "1":
+            case "2":
+                if (passNumbers) {
+                    passNumbers = false;  break;
+                }
+
+                addNameIntoLi( liAll[liAll.length - 1] );
+                console.log("added a child node.");
+                // var nameNode = document.createTextNode(tmpName);
+                // console.log("ID: "+nameNode+" / Sub: "+lengthSub);
+                //---------------------------
+                // liAll[liAll.length - lengthSub].appendChild(nameNode);
+                // lengthSub+=1;
+                // rollback = true;
+                break;
+            default:
+                console.log("ERROR!!!");
+        }
+
+        /*
         if ( 0 == key ) {
-          console.log("Running 0 key, the key is: "+key);
+          console.log("0 == key, add ul, the v is: "+value);
             ulAll[ulAll.length - 1].appendChild(ul);
           console.log("Now we got "+ulAll.length+" ul");
+          ul.appendChild(li);
+          layer += 1;
         } else {
-          console.log("Running li");
+          console.log("key= "+key+", v is: "+value);
             // var liAll = document.getElementsByTagName("li");
             // liAll[liAll.length - 1].innerHTML = value;
             if ( "name" == key ) {
-                li.innerHTML = value;
-                ulAll[ulAll.length - lengthSub].appendChild(li);
-            } else if ( "data" == key && value == null ) {
-                lengthSub++;
+                tmpName = value;
+          console.log("set tmpName ="+tmpName);
+            }
+
+            if ( "data" == key && value == null ||
+                  1 == key || 2 == key ) {
+                    //nullData = true;
+                //li.innerHTML = value;
+                var nameNode = document.createTextNode(tmpName);
+  console.log("ID: "+nameNode+" / Sub: "+lengthSub);
+                liAll[liAll.length - lengthSub].appendChild(nameNode);
+                // lengthSub+=1;
+                // var LiTarget;
+                // for (var i = 0; i < layer; i++)
+                //     LiTarget = document.getElementsByTagName("li")[0];
+              //  liAll[liAll.length - lengthSub].insertAfter(nameNode, liAll[liAll.length - lengthSub]);
+                lengthSub+=1;
+                rollback = true;
+
+
+          } else {
+
+
           }
-        }
+           */
     }
 }
 
+
+function addNameIntoLi(where) {
+    var nameNode = document.createTextNode(tmpName);
+    where.appendChild(nameNode);
+}
 
 //printAndRetrieve(data, document.getElementsByTagName("body")[0]);
 
@@ -111,11 +217,15 @@ function traverse(obj,func) {
     for (var i in obj) {
         func.apply( this, [i, obj[i] ] );
         if (obj[i] !== null && typeof(obj[i])=="object") {
-            console.log("========================Round: "+q+" | i= "+i+" | val="+obj[i]+"========================"); q++;
+            console.log("==============Round: "+q+" | key= "+i+" | val="+obj[i]+"=============="); q++;
             //going on step down in the object tree!!
             traverse(obj[i],func);
         }
     }
+}
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 traverse(data,printList);
