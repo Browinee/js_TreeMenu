792a0a4cb07cdc49f0949ac7cbcd81cad0b2724e
Daniel <daniel.chiang@isapsolution.com>
2016-01-19 16:13:20 +0800

    kinda alike

--- part2.js
+++ part2.js
@@ -28,6 +28,15 @@ var data = [
   { name: "Human", data: [] }
 ];
 
+var data2 = [
+  { name: "Animal", data: [
+                  { name: "Mammal", data: null },
+                  { name: "Reptlle", data: [
+                          { name: "Lizard", data: null }
+                  ] } ] },
+  { name: "Human", data: [] }
+];
+
 //---------------------------------------------------------------------------------------
 
 function createLiNode(key, value) {
@@ -38,10 +47,14 @@ function createLiNode(key, value) {
     return li;
 }
 
+// function insertAfter(newNode, referenceNode) {
+//     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+// }
+
 function traverse(obj, func, pos) {
-  //debugger;
-  //console.log("~~~~~~~~ round "+q+" ~~~~~~~");
-  //q++;
+//  debugger;
+//  console.log("~~~~~~~~ round "+q+" ~~~~~~~");
+//  q++;
     for ( var i in obj ) {
         if ( "name" == i ) {
             pos.appendChild( func.apply( this, [i, obj[i] ] ) );
@@ -49,13 +62,25 @@ function traverse(obj, func, pos) {
         }
 
         if ( obj[i] !== null && typeof(obj[i]) == "object" ) {
-            var ul = document.createElement("ul");
-            pos.appendChild(ul);
-            var posNext =
-              pos.getElementsByTagName("ul")[pos.getElementsByTagName("ul").length-1];
-            console.log("next pos: "+posNext);
-            console.log("============== key= "+i+" | value="+obj[i]+"==============");
-            traverse(obj[i], func, posNext);
+
+          console.log("inside, key: "+i);
+            debugger;
+            if (i != 0 && i != 1 && i != 2) {
+                var ul = document.createElement("ul");
+                pos.insertAdjacentElement("beforeEnd", ul);
+   //debugger;
+                var posNext = pos.getElementsByTagName("ul")[pos.getElementsByTagName("ul").length-1];
+                console.log("ul added");
+                console.log("next pos: "+posNext);
+                console.log("============== key= "+i+" | value="+obj[i]+"==============");
+      //                                      debugger;
+                traverse(obj[i], func, posNext);
+            } else {
+                console.log("next pos: "+pos);
+                console.log("============== key= "+i+" | value="+obj[i]+"==============");
+ //                           debugger;
+                traverse(obj[i], func, pos);
+            }
         }
     }
 }
@@ -66,6 +91,6 @@ myBody.appendChild(newUL);
 var ul = myBody.getElementsByTagName("ul")[0];
 ul.id = "clearBorder";
 
-//var q = 0;  // ----- for debug -----
+// var q = 0;  // ----- for debug -----
 
 traverse(data,createLiNode, ul);
